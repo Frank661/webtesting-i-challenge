fa3ddd4a35c883233c64431afdf3cae41e1020a8
Cisco <francisco-hernandez@lambdastudents.com>
2020-12-08 19:56:24 -0800

    tests work

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,37 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement < 20){
+    const enhanced = item.enhancement + 1;
+
+    return { ...item, enhancement:enhanced }
+  } else {
+    return { ...item }
+  }
+  ;
 }
 
 function fail(item) {
-  return { ...item };
-}
+  if(item.enhancement < 15) {
+    const decreased = item.durability -5;
+
+    return {...item, durability: decreased }
+  }  else {
+     if (item.enhancement > 16){
+    const decreased = item.durability - 10;
+    const lowered = item.enhancement - 1;  
+    return { ...item, durability: decreased, enhancement: lowered}
+  } else {
+    const weaker = item.durability - 10;
+    return {
+      ...item, durability: weaker
+    }
+  }}}
+  
+
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,54 @@
-const enhancer = require('./enhancer.js');
+const { success, fail, repair  } = require('./enhancer.js');
 // test away!
+
+describe('enhancer functions', () => {
+    describe('repair()', () => {
+        test('returns new items with durability restored to 100', () => {
+            const item = repair({ itenName:'arrow', durability: 25, enhancement: 15});
+
+            const durability = item.durability;
+            expect(durability).toBe(100)
+        })
+    })
+
+
+
+    describe('success()', () => {
+        test('returns new items with enhancement below 20', () => {
+            const item = success({ itenName:'arrow', durability: 25, enhancement: 16});
+            const enhancement = item.enhancement
+
+            expect(enhancement).toBe(17)
+        })
+        test('returns new items with enhancement levet @ 20', () => {
+            const item = success({ itenName:'arrow', durability: 25, enhancement: 20});
+            const enhancement = item.enhancement
+
+            expect(enhancement).toBe(20)
+        })
+    })
+
+
+
+
+    describe('fail()', () => {
+        test('enhancement level is greater than 16, the enhancement level decreases by 1', () => {
+            const item = fail({ itenName:'arrow', durability: 25, enhancement: 20});
+            const enhancement = item.enhancement
+
+            expect(enhancement).toBe(19)
+        })
+        test('enhancement is 15 or more, the durability of the item is decreased by 10', () => {
+            const item = fail({ itenName:'arrow', durability: 25, enhancement: 14});
+            const enhancement = item.durability
+
+            expect(enhancement).toBe(20)
+        })
+        test('items enhancement is less than 15, the durability of the item is decreased by 5', () => {
+            const item = fail({ itenName:'arrow', durability: 25, enhancement: 14});
+            const durability = item.durability
+
+            expect(durability).toBe(20)
+        })
+    })
+});
